2507031adfcd8e6917aa06a4aedf791ed9635178
Vinícius <euphonion@tutanota.com>
2021-12-13 22:44:27 -0300

    Estatistica e Resultado

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,9 +1,32 @@
-const resultado = () => {
+import { useRouter } from 'next/router';
+import Botao from '../components/Botao';
+import Estatistica from '../components/Estatistica';
+
+import styles from '../styles/Resultado.module.css';
+
+const Resultado = () => {
+  const router = useRouter();
+
+  const total = parseInt(router.query.total as string);
+  const certas = parseInt(router.query.certas as string);
+
+  const percentual = Math.round((certas / total) * 100);
+
   return (
-    <div>
-      <h1>Resultado</h1>
+    <div className={styles.resultado}>
+      <h1>Resultado Final</h1>
+      <div style={{ display: 'flex' }}>
+        <Estatistica texto="Perguntas" valor={total} />
+        <Estatistica texto="Certas" valor={certas} corFundo="#9CD2A4" />
+        <Estatistica
+          texto="Percentual"
+          valor={`${percentual}%`}
+          corFundo="#DE6A33"
+        />
+      </div>
+      <Botao href="/" texto="Tentar Novamente" />
     </div>
   );
 };
 
-export default resultado;
+export default Resultado;
